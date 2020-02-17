39d211d6441317563436a930889029ada4b566f2
Corey Ferland <ferlandc@gmail.com>
2020-02-16 19:25:34 -0500

    header component

--- pixpals/client/src/components/Header.js
+++ pixpals/client/src/components/Header.js
@@ -3,22 +3,43 @@ import { Link } from 'react-router-dom';
 
 import Avatar from './Avatar';
 
-export default class Header extends Component{
+export default class Header extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      currency: parseInt(localStorage.getItem('currency'))
     }
+  }
+
+  getCurrency() {
+    this.setState({
+      currency: parseInt(localStorage.getItem('currency'))
+    })
   }
 
   componentDidMount() {
 
   }
 
+
   render() {
     return (
       <header>
-        <Avatar currentAvatar={this.props.currentAvatar} />
+        <div className="container">
+          <div className="user-box">
+            <Avatar currentAvatar={this.props.currentAvatar} />
+          </div>
+          <div className="stats">
+            <h2>{this.props.currentUser.username}</h2>
+            <h4>{this.state.currency} Pixels</h4>
+          </div>
+        </div>
+        <nav>
+          <div><Link to="/gallery">Gallery</Link></div>
+          <div><Link to="/shop">Shop</Link></div>
+          <div><Link to="/inventory">Inventory</Link></div>
+        </nav>
       </header>
     )
   }
