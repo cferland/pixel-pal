14994bf38944a16f22959e3e764d99867f2cffc4
Corey Ferland <ferlandc@gmail.com>
2020-02-18 11:17:50 -0500

    login component error handling

--- pixpals/client/src/components/Login.js
+++ pixpals/client/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 export default class Login extends Component {
   constructor(props) {
@@ -18,25 +18,30 @@ export default class Login extends Component {
 
   render() {
     return (
-      <form onSubmit={(e) => this.props.handleLogin(e, this.state)}>
-        <h2>Login</h2>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          value={this.state.email}
-          onChange={this.handleChange}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={this.state.password}
-          onChange={this.handleChange}
-        />
-        <button>Submit</button>
-        <Link to="/register">Register</Link>
-      </form>
+      <div>
+        {this.props.currentUser ? <Redirect to={`/profile/${this.props.currentUser.username}`} /> :
+          <div className="user-form">
+            {this.props.errorText && <p className="error">{this.props.errorText}</p>}
+            <form onSubmit={(e) => this.props.handleLogin(e, this.state)}>
+              <h2>Login</h2>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+              <button>Submit</button>
+            </form>
+          </div>}
+        </div>
     )
   }
 }
