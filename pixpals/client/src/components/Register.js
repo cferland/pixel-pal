a91901471d8ff2fbab640e6a7878cab470032697
Corey Ferland <ferlandc@gmail.com>
2020-02-18 11:17:26 -0500

    register component error handling

--- pixpals/client/src/components/Register.js
+++ pixpals/client/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 export default class Register extends Component {
   constructor(props) {
@@ -21,32 +22,35 @@ export default class Register extends Component {
   render() {
     return (
       <div>
-        {this.props.errorText && <p>{this.props.errorText}</p>}
-        <form onSubmit={(e) => this.props.handleRegister(e, this.state)}>
-          <h2>Register</h2>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <button>Submit</button>
-        </form>
+        {this.props.currentUser ? <Redirect to="/" /> :
+          <div className="user-form">
+            {this.props.errorText && <p className="error">{this.props.errorText}</p>}
+            <form onSubmit={(e) => this.props.handleRegister(e, this.state)}>
+              <h2>Register</h2>
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+              <button>Submit</button>
+            </form>
+          </div>}
       </div>
     )
   }
