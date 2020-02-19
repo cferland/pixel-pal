9b871050829d5c4efc85005bc2ea9a881b73b896
Corey Ferland <ferlandc@gmail.com>
2020-02-19 10:18:37 -0500

    added seed data

--- pixpals/db/seeds.rb
+++ pixpals/db/seeds.rb
@@ -14,16 +14,20 @@ items = Item.create(
     {name: 'Hitchhiker Boots', cost: 230, image: '/images/outfits/hitchhiker.png'},
     {name: 'Modern Vampire', cost: 500, image: '/images/outfits/vamp_purple.png'},
     {name: 'Hollywood Dress', cost: 1000, image: '/images/outfits/dress_red.png'},
+    {name: 'Kill Bill', cost: 940, image: '/images/outfits/kill_bill.png'},
     {name: 'Pippy Longstocking', cost: 860, image: '/images/hairstyles/pippy_red.png'},
     {name: 'Buzzcut', cost: 50, image: '/images/hairstyles/buzz_yellow.png'},
     {name: 'Low Bun', cost: 350, image: '/images/hairstyles/bun_green.png'},
+    {name: 'Retro Wig', cost: 270, image: '/images/hairstyles/flip_pink.png'},
     {name: 'Ocean Waves', cost: 700, image: '/images/hairstyles/wavy_blue.png'},
+    {name: 'Orange Shag', cost: 90, image: '/images/hairstyles/shag_orange.png'},
+    {name: 'Ponytail', cost: 190, image: '/images/hairstyles/pony_brown.png'},
     {name: 'Cowboy Hat', cost: 420, image: '/images/hairstyles/cowboy_hat.png'},
     {name: 'Light Skin', cost: 30, image: '/images/bases/base_light.png'},
     {name: 'Dark Skin', cost: 30, image: '/images/bases/base_dark.png'},
-    {name: 'Alien', cost: 300, image: '/images/bases/base_alien.png'},
-    {name: 'Undine', cost: 500, image: '/images/bases/base_undine.png'},
-    {name: 'Drow', cost: 100, image: '/images/bases/base_grey.png'}
+    {name: 'Alien', cost: 550, image: '/images/bases/base_alien.png'},
+    {name: 'Undine', cost: 680, image: '/images/bases/base_undine.png'},
+    {name: 'Drow', cost: 300, image: '/images/bases/base_grey.png'}
   ]
 )
 
