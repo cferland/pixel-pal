1925a5cad808c14aa9ba4308406539fc3947b771
Corey Ferland <ferlandc@gmail.com>
2020-02-20 10:16:03 -0500

    test

--- pixpals/db/migrate/20200211154208_remove_avatar_from_users.rb
+++ pixpals/db/migrate/20200211154208_remove_avatar_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveAvatarFromUsers < ActiveRecord::Migration[6.0]
   def change
-    remove_reference :users, :avatar, null: false, foreign_key: true
+    # remove_reference :users, :avatar, null: false, foreign_key: true
   end
 end
